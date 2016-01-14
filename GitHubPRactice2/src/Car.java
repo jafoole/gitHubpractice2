07e74bdc8a8883449cd002e196211d61f407b6c2
jafoole <smallbabba@gmail.com>
2016-01-14 12:38:10 -0500

    first

---
+++ GitHubPRactice2/src/Car.java
@@ -0,0 +1,13 @@
+/**
+ * Created by sbabba on 1/14/16.
+ */
+public class Car {
+    public static void main(String[] args) {
+
+
+    }
+
+    public int numDoors;
+    public int maxSpeed;
+    public int minSpeed;
+}
---
+++ GitHubPRactice2/src/Garage.java
@@ -0,0 +1,8 @@
+/**
+ * Created by sbabba on 1/14/16.
+ */
+public class Garage {
+    public static void main(String[] args) {
+
+    }
+}
